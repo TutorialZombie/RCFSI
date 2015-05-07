b9472776f72eb947a04f77b4920c49815bb891b0
TutorialZombie <noemail@noemail.com>
2015-05-06 20:41:43 -0400

    Added permission to use rcfsi

--- RightClickForSpawnerID/src/me/TutorialZombie/RCFSI/Main.java
+++ RightClickForSpawnerID/src/me/TutorialZombie/RCFSI/Main.java
@@ -9,6 +9,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener{
@@ -23,8 +24,10 @@ public class Main extends JavaPlugin implements Listener{
 	public void onPlayerInteract(PlayerInteractEvent e){
 		if(e.getAction() == Action.RIGHT_CLICK_BLOCK){
 			if(e.getClickedBlock().getType() == Material.MOB_SPAWNER){
-				CreatureSpawner cs = (CreatureSpawner) e.getClickedBlock().getState();
-				e.getPlayer().sendMessage(ChatColor.GREEN + cs.getCreatureTypeName() +" Spawner" + ChatColor.AQUA + " " + cs.getTypeId() + ":" + cs.getSpawnedType().getTypeId());
+				if(e.getPlayer().hasPermission(new Permission("RCFSI"))){
+					CreatureSpawner cs = (CreatureSpawner) e.getClickedBlock().getState();
+					e.getPlayer().sendMessage(ChatColor.GREEN + cs.getCreatureTypeName() +" Spawner" + ChatColor.AQUA + " " + cs.getTypeId() + ":" + cs.getSpawnedType().getTypeId());
+				}
 			}
 		}	
 	}
